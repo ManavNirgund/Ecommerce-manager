60caf75d0d4ee79f8e07ab1e0c779dfa68a2a8d6
Manavendra Nirgund <manavjnirgund@gmail.com>
2020-11-28 16:37:26 +0530

    Files Modified

--- EComm/main.py
+++ EComm/main.py
@@ -6,12 +6,14 @@ from PyQt5 import QtWidgets, uic
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
 
-    win1 = uic.loadUi("mainwindow.ui")
-    win2 = uic.loadUi("cuslogin.ui")
-    win3 = uic.loadUi("sellogin.ui")
+    win1 = uic.loadUi("login.ui")
+    win2 = uic.loadUi("cussignup.ui")
+    win3 = uic.loadUi("selsignup.ui")
+    win4 = uic.loadUi("wrongcred.ui")
 
     win1.show()
     win2.show()
     win3.show()
+    win4.show()
 
     app.exec()
