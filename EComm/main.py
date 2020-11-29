caef796d8543c7e46f21da01f58a00f2d413f600
Nikit <nikit.periwal@gmail.com>
2020-11-29 12:45:47 +0530

    Removed wrongcred.ui

--- EComm/main.py
+++ EComm/main.py
@@ -5,15 +5,10 @@ from PyQt5 import QtWidgets, uic
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-
     win1 = uic.loadUi("login.ui")
     win2 = uic.loadUi("cussignup.ui")
     win3 = uic.loadUi("selsignup.ui")
-    win4 = uic.loadUi("wrongcred.ui")
-
     win1.show()
     win2.show()
     win3.show()
-    win4.show()
-
     app.exec()
