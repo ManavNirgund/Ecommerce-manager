41a37df7c86a8eac928637f2c629f20bd29429ec
Manavendra Nirgund <manavjnirgund@gmail.com>
2020-11-30 19:09:43 +0530

    Update Files

--- EComm/main.py
+++ EComm/main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5 import QtWidgets, uic
-
+#from login_screen import Ui_Dialog
 
 def showLogin():
     win = uic.loadUi("login_screen.ui")
@@ -19,9 +19,33 @@ def showCustomerSignup():
     win.show()
     return win
 
+def showCustomerWindow():
+    win = uic.loadUi("customerwindow.ui")
+    win.show()
+    return win
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    window = showSellerSignup()
+    window = showLogin()
     if window.exec():
         app.exec()
+
+'''if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = showCustomerWindow()
+    window.show()
+    app.exec()'''
+
+'''class MainWindow(QtWidgets.QMainWindow, Ui_Dialog):
+    def __init__(self, *args, obj=None, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+
+
+app = QtWidgets.QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+app.exec()
+'''
