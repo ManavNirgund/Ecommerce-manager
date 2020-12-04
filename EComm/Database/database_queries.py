00bf932c8b2e13165500a5ec3a77cebbddc63131
Nikit <nikit.periwal@gmail.com>
2020-12-04 19:50:52 +0530

    Updated database_queries.py.py

--- EComm/Database/database_queries.py
+++ EComm/Database/database_queries.py
@@ -13,31 +13,21 @@ def verify_user(conn, email, password):
         email: the email of the user
         password: the password of the user
     Returns
-        seller_row, "Seller": If user exists in seller, and password is verified.
-        customer_row, "Customer": If user exists in customer, and password is verified.
+        row: If user exists, and password is verified.
         "Incorrect Password": If user exists, but password is incorrect.
         "No Record Found": If user doesn't exist.
     """
     try:
         # SQL command to get user if it exists.
-        sql = "SELECT * FROM customer WHERE email= ? ;"
+        sql = "SELECT * FROM user WHERE email= ? ;"
         cur = conn.cursor()
         cur.execute(sql, (email, ))
-        customer_row = cur.fetchone()
-        sql = "SELECT * FROM seller WHERE email= ? ;"
-        cur = conn.cursor()
-        cur.execute(sql, (email,))
-        seller_row = cur.fetchone()
+        row = cur.fetchone()
 
         # Verifies the password, and returns the user details.
-        if seller_row is not None:
-            if pbkdf2_sha256.verify(password, seller_row[3]):
-                return seller_row, "Seller"
-            else:
-                return "Incorrect Password"
-        elif customer_row is not None:
-            if pbkdf2_sha256.verify(password, customer_row[3]):
-                return customer_row, "Customer"
+        if row is not None:
+            if pbkdf2_sha256.verify(password, row[3]):
+                return row
             else:
                 return "Incorrect Password"
         else:
